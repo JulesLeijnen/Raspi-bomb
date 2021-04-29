729588c5ad3d1b1182a2fa7ec141c29ad942d2ee
JulesLeijnen <60113301+JulesLeijnen@users.noreply.github.com>
2021-04-29 12:55:41 +0100

    edited blinking process to confirm concept
    all sorts of things can be hooked to this.

--- blinking_process.py
+++ blinking_process.py
@@ -10,20 +10,23 @@ Blink_message_recieved = False
 def on_message(client, userdata, message):
     global defused, B_procces_inprogress                                # B_procces_inprogress is a variable that makes sure the blinking sequence can only happen ones, and defused tells the blinker to stop.
     new_message = json.loads(str(message.payload.decode("utf-8")))
-    logging.info("message received: {}\nMessage topic={}\nMessage qos={}\nMessage retain flag={}\nMessage type={}\n".format(new_message, message.topic, message.qos, message.retain, type(message)))
+    print("message received: {}\nMessage topic={}\nMessage qos={}\nMessage retain flag={}\nMessage type={}\n".format(new_message, message.topic, message.qos, message.retain, type(message)))
     if new_message[1] == "CLEARED":
         defused = True
     if new_message[1] == "B" and not B_procces_inprogress:
         B_procces_inprogress = True
         until(new_message[4] + new_message[2] - new_message[3])
+        print("UNTILL over")
         currently_blinking = False
         while not defused:
             until(time() + 0.5)
-            currently_blinking != currently_blinking
+            currently_blinking = not currently_blinking
             if currently_blinking:
                 gpio.output(22, gpio.HIGH)
+                print("HIGH")
             elif not currently_blinking:
                 gpio.output(22, gpio.LOW)
+                print("LOW")
             pass
 
 def blinking_PRE_process(DEBUG):
@@ -39,17 +42,17 @@ def blinking_PRE_process(DEBUG):
     
     B_procces_inprogress = False
 
-    blinkpin = 25
+    blinkpin = 22
 
     #Setup GPIO stuff
     gpio.setmode(gpio.BOARD)
     gpio.setwarnings(DEBUG)
-    gpio.setup(blinkpin)
+    gpio.setup(blinkpin, gpio.OUT)
 
     while not defused:
         sleep(5)
     exit(0)
 
-
+blinking_PRE_process(1)
 
 
